18fff9accc547155423e449816e4c6a76f95c74d
Natalie Ammerman <neammerman@gmail.com>
2025-07-11 15:37:45 -0400

    Change duration from label to seconds

--- pkg/cmd/query/event_printer.go
+++ pkg/cmd/query/event_printer.go
@@ -88,7 +88,7 @@ func printOpenMetricsTimestamps(events []*auditv1.Event, w io.Writer) error {
 		code := e.ResponseStatus.Code
 		timeStamp := e.RequestReceivedTimestamp.Time.Unix()
 		stage := e.Stage
-		duration := e.StageTimestamp.Sub(e.RequestReceivedTimestamp.Time)
+		duration := e.StageTimestamp.Sub(e.RequestReceivedTimestamp.Time).Seconds()
 
 		resource := ""
 		subresource := ""
@@ -104,7 +104,7 @@ func printOpenMetricsTimestamps(events []*auditv1.Event, w io.Writer) error {
 			uid = string(e.ObjectRef.UID)
 		}
 
-		fmt.Fprintf(w, "audit_event_timestamp{user=\"%s\",verb=\"%s\",resource=\"%s\",subresource=\"%s\",name=\"%s\",namespace=\"%s\",uid=\"%s\",stage=\"%s\",duration=\"%s\",code=\"%d\"} 1 %d\n", user, verb, resource, subresource, name, namespace, uid, stage, duration, code, timeStamp)
+		fmt.Fprintf(w, "audit_event_duration_seconds{user=\"%s\",verb=\"%s\",resource=\"%s\",subresource=\"%s\",name=\"%s\",namespace=\"%s\",uid=\"%s\",stage=\"%s\",code=\"%d\"} %d %d\n", user, verb, resource, subresource, name, namespace, uid, stage, code, duration, timeStamp)
 	}
 	fmt.Fprintln(w, "# EOF")
 	return nil
